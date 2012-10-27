12597362e896b8c3085a7997d36f7753743a2d03
fumin <awawfumin@gmail.com>
2012-10-28 02:03:36 +0800

    debug session

--- auth.rb
+++ auth.rb
@@ -23,6 +23,7 @@ class Auth
                    "&state=#{@request.session['state']}"
       return [200, {}, ["<script>top.location.href='#{dialog_url}'</script>"]]
     end
+puts "session: #{@request.session['state']}, params: #{@request.params['state']}"
     if @request.session['state'] and @request.session['state'] == @request.params['state']
       response = RC::Universal.new.get("https://graph.facebook.com/oauth/access_token",
                                        client_id: APP_ID, redirect_uri: LOGIN_URL,
